bd30a195024d12c976014fb17be964450a57e2b5
RamshackleJamathon <josephvandrunen@gmail.com>
2016-08-16 16:13:35 -0400

    init

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# gl-flightControls
+# gl-flyCamera
 ✈️ controls
 
+## Installation
+
+```
+$ npm install gl-flyCamera
+```
+
+## Example
+
+```javascript
+controls = new flyCamera();
+controls.start();
+
+function renderLoop(){
+	controls.update();
+	var currentPosition = controls.position; //current camera position
+	var currentOrientation = controls.quaternion; //current camera orientation
+	/*
+
+		...doing the actual rendering
+
+	*/
+	window.requestAnimationFrame(renderLoop);
+}
+window.requestAnimationFrame(renderLoop);
+
+```
+
+## Badges
+
+![](https://img.shields.io/badge/license-MIT-blue.svg)
+![](https://img.shields.io/badge/status-developing-yellow.svg)
---
+++ index.js
@@ -0,0 +1,197 @@
+var glMatrix = require('gl-matrix');
+
+var flyCamera = function( domElement ) {
+	this.domElement = ( domElement !== undefined ) ? domElement : document;
+	if ( domElement ) this.domElement.setAttribute( 'tabindex', -1 );
+
+	this.movementSpeed = 1.0;
+	this.rollSpeed = 0.05;
+	this.dragToLook = false;
+	this.autoForward = false;
+	this.paused = false;
+	this.mouseStatus = 0;
+
+	this.tmpQuaternion = glMatrix.quat.create();
+	this.moveVector = glMatrix.vec3.create();
+	this.rotationVector = glMatrix.vec3.create();
+
+	this.position = glMatrix.vec3.fromValues( 1, 6, 0);
+	this.quaternion = glMatrix.quat.create();
+
+	this.lastUpdate = Date.now();
+
+	this.moveState = { 
+		up: 0, 
+		down: 0, 
+		left: 0, 
+		right: 0, 
+		forward: 0, 
+		back: 0, 
+		pitchUp: 0, 
+		pitchDown: 0, 
+		yawLeft: 0, 
+		yawRight: 0, 
+		rollLeft: 0, 
+		rollRight: 0 
+	};
+}
+
+flyCamera.prototype.keydown = function (event) {
+	if ( event.altKey ) {
+			return;
+	}
+	event.preventDefault();
+	switch ( event.keyCode ) {
+		case 16: /* shift */ this.movementSpeedMultiplier = .1; break;
+		case 87: /*W*/ this.moveState.forward = 1; break;
+		case 83: /*S*/ this.moveState.back = 1; break;
+		case 65: /*A*/ this.moveState.left = 1; break;
+		case 68: /*D*/ this.moveState.right = 1; break;
+		case 82: /*R*/ this.moveState.up = 1; break;
+		case 70: /*F*/ this.moveState.down = 1; break;
+		case 38: /*up*/ this.moveState.pitchUp = 1; break;
+		case 40: /*down*/ this.moveState.pitchDown = 1; break;
+		case 37: /*left*/ this.moveState.yawLeft = 1; break;
+		case 39: /*right*/ this.moveState.yawRight = 1; break;
+		case 81: /*Q*/ this.moveState.rollLeft = 1; break;
+		case 69: /*E*/ this.moveState.rollRight = 1; break;
+	}
+	this.updateMovementVector();
+	this.updateRotationVector();
+}
+
+flyCamera.prototype.keyup = function (event) {
+	switch ( event.keyCode ) {
+		case 16: /* shift */ this.movementSpeedMultiplier = 1; break;
+		case 87: /*W*/ this.moveState.forward = 0; break;
+		case 83: /*S*/ this.moveState.back = 0; break;
+		case 65: /*A*/ this.moveState.left = 0; break;
+		case 68: /*D*/ this.moveState.right = 0; break;
+		case 82: /*R*/ this.moveState.up = 0; break;
+		case 70: /*F*/ this.moveState.down = 0; break;
+		case 38: /*up*/ this.moveState.pitchUp = 0; break;
+		case 40: /*down*/ this.moveState.pitchDown = 0; break;
+		case 37: /*left*/ this.moveState.yawLeft = 0; break;
+		case 39: /*right*/ this.moveState.yawRight = 0; break;
+		case 81: /*Q*/ this.moveState.rollLeft = 0; break;
+		case 69: /*E*/ this.moveState.rollRight = 0; break;
+	}
+	this.updateMovementVector();
+	this.updateRotationVector();
+};
+
+flyCamera.prototype.mousemove = function (event) {
+	if ( !this.dragToLook || this.mouseStatus > 0 ) {
+		var container = this.getContainerDimensions();
+		var halfWidth  = container.size[ 0 ] / 2;
+		var halfHeight = container.size[ 1 ] / 2;
+		this.moveState.yawLeft   = - ( ( event.pageX - container.offset[ 0 ] ) - halfWidth  ) / halfWidth;
+		this.moveState.pitchDown =   ( ( event.pageY - container.offset[ 1 ] ) - halfHeight ) / halfHeight;
+		this.updateRotationVector();
+	}
+};
+
+flyCamera.prototype.mousedown = function (event) {
+	if ( this.domElement !== document ) {
+		this.domElement.focus();
+	}
+	event.preventDefault();
+	event.stopPropagation();
+	if ( this.dragToLook ) {
+		this.mouseStatus ++;
+	} else {
+		switch ( event.button ) {
+			case 0: this.moveState.forward = 1; break;
+			case 2: this.moveState.back = 1; break;
+		}
+		this.updateMovementVector();
+	}
+};
+
+flyCamera.prototype.mouseup = function (event) {
+	event.preventDefault();
+	event.stopPropagation();
+	if ( this.dragToLook ) {
+		this.mouseStatus --;
+		this.moveState.yawLeft = this.moveState.pitchDown = 0;
+	} else {
+		switch ( event.button ) {
+			case 0: this.moveState.forward = 0; break;
+			case 2: this.moveState.back = 0; break;
+		}
+		this.updateMovementVector();
+	}
+	this.updateRotationVector();
+};
+
+flyCamera.prototype.update = function () {
+	if (!this.paused){
+		this.updateMovementVector();
+		this.updateRotationVector();
+
+		var now = Date.now();
+		var delta = (now - this.lastUpdate) / 100;
+		this.lastUpdate = now;
+
+		var moveMult = delta * this.movementSpeed;
+		var rotMult  = delta * this.rollSpeed;
+
+
+		glMatrix.vec3.scale( this.rotationVector, this.rotationVector, rotMult );
+		this.tmpQuaternion = glMatrix.quat.fromValues(this.rotationVector[0],this.rotationVector[1],this.rotationVector[2], 1);
+		glMatrix.quat.normalize(this.tmpQuaternion,this.tmpQuaternion);
+		glMatrix.quat.mul(this.quaternion, this.quaternion, this.tmpQuaternion);
+
+		glMatrix.vec3.scale( this.moveVector, this.moveVector, moveMult );
+		glMatrix.vec3.transformQuat( this.moveVector,  this.moveVector, this.quaternion );
+		glMatrix.vec3.add( this.position, this.position, this.moveVector );
+	}
+};
+
+flyCamera.prototype.updateMovementVector = function () {
+	if (!this.paused){
+		var forward = ( this.moveState.forward || ( this.autoForward && !this.moveState.back ) ) ? 1 : 0;
+		this.moveVector = glMatrix.vec3.fromValues(
+			-this.moveState.left    + this.moveState.right,
+			-this.moveState.down    + this.moveState.up,
+			-forward + this.moveState.back
+		);
+	}
+};
+
+flyCamera.prototype.updateRotationVector = function () {
+	if (!this.paused){
+		this.rotationVector = glMatrix.vec3.fromValues(
+			-this.moveState.pitchDown + this.moveState.pitchUp,
+			-this.moveState.yawRight  + this.moveState.yawLeft,
+			-this.moveState.rollRight + this.moveState.rollLeft
+		);
+	}
+};
+
+flyCamera.prototype.getContainerDimensions = function () {
+	if ( this.domElement != document ) {
+		return {
+			size  : [ this.domElement.offsetWidth, this.domElement.offsetHeight ],
+			offset  : [ this.domElement.offsetLeft,  this.domElement.offsetTop ]
+		};
+	} else {
+		return {
+			size  : [ window.innerWidth, window.innerHeight ],
+			offset  : [ 0, 0 ]
+		};
+	}
+};
+
+flyCamera.prototype.start = function () {
+	document.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); } );
+	document.addEventListener( 'mousemove', this.mousemove.bind(this) );
+	document.addEventListener( 'mousedown', this.mousedown.bind(this) );
+	document.addEventListener( 'mouseup',   this.mouseup.bind(this) );
+	window.addEventListener( 'keydown', this.keydown.bind(this) );
+	window.addEventListener( 'keyup',   this.keyup.bind(this) );
+	this.updateMovementVector();
+	this.updateRotationVector();
+}
+
+module.exports = flyCamera;
